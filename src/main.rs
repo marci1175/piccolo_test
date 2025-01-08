fa80b74c08f39d45f0057dad71bc329c2ca913e6
Varga Marcell <128537619+marci1175@users.noreply.github.com>
2025-01-09 00:12:48 +0100

    yes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::{atomic::AtomicI32, Arc};
 
-use piccolo::{Callback, Closure, Executor, Lua};
+use piccolo::{error::LuaError, Callback, Closure, Executor, Function, Lua, RuntimeError, Value};
 
 fn main() {
     let shared_data = Arc::new(AtomicI32::new(0));
@@ -10,7 +10,7 @@ fn main() {
     let mut lua = Lua::core();
 
     lua.load_io();
-
+    
     lua.enter(|ctx| {
         let callback = Callback::from_fn(&ctx, move |ctx, executor, stack| {
             shared_data_clone.store(240, std::sync::atomic::Ordering::Relaxed);
@@ -18,11 +18,25 @@ fn main() {
             Ok(piccolo::CallbackReturn::Return)
         });
 
+        let print = Callback::from_fn(&ctx, |a, b, mut c| {
+            let arg_value = c.pop_front();
+
+            if !arg_value.is_nil() {
+                println!("{}", arg_value.to_string());
+            }
+            else {
+                return Err(piccolo::Error::Lua(LuaError::from(Value::Nil)));
+            }
+
+            Ok(piccolo::CallbackReturn::Return)
+        });
+
         ctx.globals().set(ctx, "data_store_test", callback).unwrap();
+        ctx.globals().set(ctx, "fasz", print).unwrap();
     });
 
     let executor = lua.enter(|ctx| {
-        let closure = Closure::load(ctx, None, &br#"data_store_test(); print("Hello from Lua!")"#[..]).unwrap();
+        let closure = Closure::load(ctx, None, &br#"fasz("apadxd"); print("Hello from Lua!")"#[..]).unwrap();
 
         ctx.stash(Executor::start(
             ctx,
